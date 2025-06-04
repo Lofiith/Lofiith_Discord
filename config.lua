e187394485eb047d1daa55c0ec638f7f93a10573
Lofiith <148030987+Lofiith@users.noreply.github.com>
2025-06-03 23:40:20 -0500

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,8 +1,8 @@
 -- Fully server-sided, modders cannot dump it.
 
 Config = {
-    bot_token = "MTM0MjM5MTA4NDk3Nzg4MTE0OQ.GPKYq4.1IpjSn0kueF7J40RsG8bNs5XPK1g4A24wsrf9c",
-    guild_id = "1259765678374916106",
+    bot_token = "PUT_BOT_TOKEN_HERE", -- Your Discord bot token
+    guild_id = "PUT_GUILD_ID_HERE", -- Your Discord server (guild) ID
     debug = true, -- Set to false to disable debug messages in the console   
     timeout = 5, -- Timeout in seconds for Discord API requests
     kick_not_in_discord = true, -- Set to true to kick players not in the Discord server
